d623317abf377ff0eb75c28b69d4eeb5eb19da0b
test-user <test@user.com>
2017-03-19 22:48:18 +0800

    setup separate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-if (process.env.HEROKU_USING === 'YES') {
-  mongoose.connect(process.env.MONGOLAB_URI);
-} else if (process.env.HEROKU_USING === 'NO') {
-  mongoose.connect('mongodb://localhost:27017/todoapp');
-}
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
