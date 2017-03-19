6911b72e7c4001bb3bd3d43d27351d5e521f2fac
test-user <test@user.com>
2017-03-19 18:15:44 +0800

    updated port for local mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 if (process.env.HEROKU_USING === 'YES') {
   mongoose.connect(process.env.MONGOLAB_URI);
 } else if (process.env.HEROKU_USING === 'NO') {
-  mongoose.connect('mongodb://localhost:3000/todoapp');
+  mongoose.connect('mongodb://localhost:27017/todoapp');
 }
 
 module.exports = { mongoose };
